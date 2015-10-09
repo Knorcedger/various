53565105f1ed28b71c77b8829a56c117d9469ba5
atsoumitas <tsoumitas@thinkdesquared.com>
2015-10-09 16:36:50 +0300

    Added mood.js and excluded it from jscs

---
+++ mood.js
@@ -0,0 +1,14 @@
+var me = {
+	firstname: 'Achilleas',
+	surname: 'Tsoumitas',
+	nickname: 'Knorcedger',
+	mood: howAreWeToday()
+};
+
+if (me.mood === 'sad') {
+	offerCookie();
+} else if (me.mood === 'happy') {
+	offerChallenge();
+}
+
+< "Challenge Accepted"
